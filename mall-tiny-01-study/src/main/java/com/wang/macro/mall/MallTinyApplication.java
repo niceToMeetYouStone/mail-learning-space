e5011048d325348500465126fabd2b51a558f0f7
石头 <2398391057@qq.com>
2022-08-17 11:03:44 +0800

    初始化项目

---
+++ mall-tiny-01-study/src/main/java/com/wang/macro/mall/MallTinyApplication.java
@@ -0,0 +1,14 @@
+package com.wang.macro.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class MallTinyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyApplication.class,args);
+    }
+}
